e64a6be5a19e50ccfc0600c28325365f8d82f8c4
Frederico Baker <70851418+FredericoBaker@users.noreply.github.com>
2024-06-24 17:02:24 -0300

    test: add Cypress tests for editing, toggling and deleting tasks

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -56,4 +56,76 @@ describe('template spec', () => {
     cy.get('.todo-list li')
       .should('have.length', 2);
   });
+
+  // ----- Novos testes implementados -----
+  
+  it('Edita uma tarefa e marca como completa', () => {
+    cy.visit('http://127.0.0.1:7001');
+
+    cy.get('.new-todo')
+      .type('Tarefa para editar{enter}');
+
+    // Edita a tarefa
+    cy.get('.todo-list li')
+      .dblclick()
+      .find('.edit')
+      .clear()
+      .type('Tarefa editada{enter}');
+
+    // Marca a tarefa como completa
+    cy.get('.todo-list li .toggle')
+      .click();
+
+    cy.get('.todo-list li')
+      .should('have.length', 1)
+      .first()
+      .should('have.text', 'Tarefa editada')
+      .and('have.class', 'completed');
+  });
+
+  it('Limpa todas as tarefas completas', () => {
+    cy.visit('http://127.0.0.1:7001');
+
+    cy.get('.new-todo')
+      .type('Tarefa a ser completada{enter}')
+      .type('Outra tarefa{enter}');
+
+    cy.get('.todo-list li .toggle')
+      .first()
+      .click();
+
+    cy.get('.clear-completed').click();
+    cy.get('.todo-list li')
+      .should('have.length', 1)
+      .first()
+      .should('have.text', 'Outra tarefa');
+  });
+
+  it('Marca uma tarefa como completa e depois desmarca', () => {
+    cy.visit('http://127.0.0.1:7001');
+
+    cy.get('.new-todo')
+      .type('Tarefa para completar{enter}');
+
+    // Marca a tarefa como completa
+    cy.get('.todo-list li .toggle')
+      .click();
+
+    // Verifica se a tarefa está marcada como completa
+    cy.get('.todo-list li')
+      .should('have.length', 1)
+      .first()
+      .should('have.class', 'completed');
+
+    // Desmarca a tarefa
+    cy.get('.todo-list li .toggle')
+      .click();
+
+    // Verifica se a tarefa está desmarcada
+    cy.get('.todo-list li')
+      .should('have.length', 1)
+      .first()
+      .should('not.have.class', 'completed');
+  });
+
 });
